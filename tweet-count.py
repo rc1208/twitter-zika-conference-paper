f73a61efbeab1a4a437fcef52ce8c8d9f42fd37b
Rahul Chowdhury <chowdhuryrahul5@gmail.com>
2016-06-30 20:14:59 +0530

    parsed data and added csv file

--- tweet-count.py
+++ tweet-count.py
@@ -11,23 +11,41 @@ for line in tweets_file:
         tweets_data.append(tweet)
     except:
         continue
-print (len(tweets_data))
 
-for tweets in tweets_data :
-	print(tweets['coordinates']['coordinates'])
 
-'''
+
 tweets = pd.DataFrame()
-#mapping for all except user and entities
-for attr in tweets_data[0]:
-	if attr != 'user' and attr != 'entities':
-		tweets[attr] = map(lambda tweet: tweet[attr], tweets_data) 
-#mapping for entities
-tweets['expanded_url'] = map(lambda tweet: tweet['entities']['urls']['expanded_url'],tweets_data)
-tweets['hashtags'] = map(lambda tweet: tweet['entities']['hashtags']['text'],tweets_data)
-#mapping for user
-
+#mapping text
+tweets['text'] = list(map(lambda tweet: tweet['text'],tweets_data))
 '''
+for t in tweets['text']:
+	print(t)
+'''
+#mapping created_at 
+tweets['created_at'] = list(map(lambda tweet: tweet['created_at'],tweets_data))
+'''
+for t in tweets['created_at']:
+	print(t)
+'''
+#id_string
+tweets['id_str'] = list(map(lambda tweet: tweet['id_str'],tweets_data))
 
+#users
+tweets['user_id'] = list(map(lambda tweet: tweet['user']['id_str'],tweets_data))
+tweets['user_location'] = list(map(lambda tweet: tweet['user']['location'],tweets_data))
+tweets['user_followers_count'] = list(map(lambda tweet: tweet['user']['followers_count'],tweets_data))
+tweets['user_friends_count'] = list(map(lambda tweet: tweet['user']['friends_count'],tweets_data))
+tweets['user_followers_count'] = list(map(lambda tweet: tweet['user']['followers_count'],tweets_data))
+tweets['user_favourites_count'] = list(map(lambda tweet: tweet['user']['favourites_count'],tweets_data))
+
+#retweets
+tweets['retweet_count'] = list(map(lambda tweet: tweet['retweet_count'],tweets_data))
+#fav_count
+tweets['favorite_count'] = list(map(lambda tweet: tweet['favorite_count'],tweets_data))
+#retweet_count
+tweets['retweet_count'] = list(map(lambda tweet: tweet['retweet_count'],tweets_data))
+
+#converting to csv file
+tweets.to_csv('tweets.csv')
 
 
